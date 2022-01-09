9511c11c6e67b6b827aac380f775eac5ecf01963
blackrose514 <dmtri310@tutanota.com>
2022-01-09 23:03:10 +0200

    remove JSON.stringfy

--- src/utils/setCache.ts
+++ src/utils/setCache.ts
@@ -3,7 +3,7 @@ import { redis } from "../services"
 export default async () => {
   const latestUpdatedPosts = (await redis.get("latest-update")) || "[]"
 
-  await redis.setex("cache", 60 * 5, JSON.stringify(latestUpdatedPosts))
+  await redis.setex("cache", 60 * 5, latestUpdatedPosts)
 
   return latestUpdatedPosts
 }
